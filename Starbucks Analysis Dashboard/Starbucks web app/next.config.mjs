1795cf661667c365de4de6ea0fa44ae2f6238c0c
theaditya24 <adityaraj240203@gmail.com>
2026-08-13 02:16:18 +0530

    Fix Next.js path alias for Render

--- Starbucks Analysis Dashboard/Starbucks web app/next.config.mjs
+++ Starbucks Analysis Dashboard/Starbucks web app/next.config.mjs
@@ -1,6 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 /** @type {import('next').NextConfig} */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config) => {
+    config.resolve.alias['@'] = __dirname;
+    return config;
+  },
 };
 
 export default nextConfig;
